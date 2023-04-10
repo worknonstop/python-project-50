f4d5124b87bc665403be038da48ed22af9f29fa7
Yuriy Y <monkcrazy91@gmail.com>
2023-04-10 22:47:46 +0600

    add json files

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -6,6 +6,8 @@ from gendiff.cli import display_help
 
 def main():
     display_help()
+    diff = generate_diff("gendiff/file1.json", "gendiff/file2.json")
+    print(diff)
 
 
 if __name__ == "__main__":
