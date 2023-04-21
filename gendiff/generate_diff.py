18da71d55209b74282ced098011b885c18ec4587
Yuriy Y <monkcrazy91@gmail.com>
2023-04-21 16:20:31 +0600

    fix generate_diff.py

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -9,15 +9,15 @@ def get_diff_dict(dict1, dict2):
     for key in sorted_keys:
         if key in dict1 and key in dict2:
             if dict1[key] == dict2[key]:
-                new_dict["  " + key] = dict1[key]
+                new_dict["   " + key] = dict1[key]
             elif dict1[key] != dict2[key]:
-                new_dict["- " + key] = dict1[key]
-                new_dict["+ " + key] = dict2[key]
+                new_dict[" - " + key] = dict1[key]
+                new_dict[" + " + key] = dict2[key]
         elif key in dict1 or key in dict2:
             if key in dict1:
-                new_dict["- " + key] = dict1[key]
+                new_dict[" - " + key] = dict1[key]
             elif key in dict2:
-                new_dict["+ " + key] = dict2[key]
+                new_dict[" + " + key] = dict2[key]
     return new_dict
 
 
